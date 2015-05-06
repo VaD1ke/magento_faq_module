04ab5097da4023dc6dd5baecfccf60e1c934d2a3
Vladislav Slesarenko <vslesarenko@oggettoweb.com>
2015-05-06 10:20:28 +0300

    Change test for preparing columns in grid

--- Oggetto/Faq/Test/Block/Adminhtml/Faq/Grid.php
+++ Oggetto/Faq/Test/Block/Adminhtml/Faq/Grid.php
@@ -33,14 +33,22 @@
 class Oggetto_Faq_Test_Block_Adminhtml_Faq_Grid extends EcomDev_PHPUnit_Test_Case
 {
     /**
+     * Replace core session by mock
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->replaceByMock('singleton', 'core/session', $this->getModelMock('core/session', ['start']));
+    }
+
+    /**
      * Set question collection and add filter by answered status
      *
      * @return void
      */
     public function testSetsQuestionsCollectionAndFilterByAnsweredStatus()
     {
-        $this->replaceByMock('singleton', 'core/session', $this->getModelMock('core/session', ['start']));
-
         $questions = $this->getResourceModelMock('oggetto_faq/questions_collection', ['newOnTop']);
 
         $questions->expects($this->once())
@@ -77,132 +85,13 @@ class Oggetto_Faq_Test_Block_Adminhtml_Faq_Grid extends EcomDev_PHPUnit_Test_Cas
      */
     public function testPreparesColumns()
     {
-        $this->replaceByMock('singleton', 'core/session', $this->getModelMock('core/session', ['start']));
-
-        $block = $this->getBlockMock('oggetto_faq/adminhtml_faq_grid',
-            [ 'addColumn', '_prepareMassactionBlock']
-        );
-
-        for ($i = 0; $i < 7; $i++) {
-            $block->expects($this->at($i))
-                ->method('addColumn')
-                ->with(
-                    $this->equalTo($this->expected($i)->getId()),
-                    $this->equalTo($this->expected($i)->getParams())
-                )
-                ->willReturnSelf();
-        }
-
+        $block = new Oggetto_Faq_Block_Adminhtml_Faq_Grid;
+        $layout = new Mage_Core_Model_Layout;
+        $block->setLayout($layout);
         $block->toHtml();
-    }
 
-    /**
-     * Prepare massaction for deleting questions
-     *
-     * @return void
-     */
-    public function testPreparesMassactionForDeletingQuestions()
-    {
-        $block = $this->getBlockMock('oggetto_faq/adminhtml_faq_grid',
-            [
-                'addColumn',
-                'getLayout',
-                'getMassactionBlock',
-                'getMassactionBlockName',
-                'getUrl',
-                '_prepareCollection',
-                '_prepareColumns',
-                '_prepareMassactionColumn',
-                'setMassactionIdField',
-                'setChild',
-            ]);
+        $blockColumns = array_keys($block->getColumns());
 
-        $massBlock = $this->getBlockMock('adminhtml/widget_grid_massaction',
-            ['setFormFieldName', 'addItem', 'isAvailable']);
-
-        $massBlock->expects($this->once())
-            ->method('isAvailable')
-            ->willReturn(true);
-
-        $massBlock->expects($this->once())
-            ->method('setFormFieldName')
-            ->with($this->equalTo('questions'));
-
-        $massBlock->expects($this->at(1))
-            ->method('addItem')
-            ->with(
-                $this->equalTo('delete'),
-                $this->equalTo([
-                    'label'   => 'Delete',
-                    'url'     => '*/*/massDelete'
-                ])
-            );
-
-        $layout = $this->getModelMock('core/layout', ['createBlock']);
-
-        $block->expects($this->any())
-            ->method('setChild')
-            ->with(
-                $this->equalTo('massaction'),
-                $this->equalTo($massBlock)
-            );
-
-        $block->expects($this->any())
-            ->method('getLayout')
-            ->will($this->returnValue($layout));
-
-        $layout->expects($this->any())
-            ->method('createBlock')
-            ->with($this->equalTo('massaction'))
-            ->willReturn($massBlock);
-
-        $block->expects($this->once())
-            ->method('getMassactionBlockName')
-            ->willReturn('massaction');
-
-        $block->expects($this->any())
-            ->method('getMassactionBlock')
-            ->willReturn($massBlock);
-
-        $block->expects($this->any())
-            ->method('getUrl')
-            ->with($this->anything())
-            ->willReturnArgument(0);
-
-        $block->expects($this->once())
-            ->method('setMassactionIdField')
-            ->with($this->equalTo('question_id'));
-
-        $block->toHtml();
-    }
-
-
-    /**
-     * Check url for rows in grid
-     *
-     * @return void
-     */
-    public function testChecksUrlForRowInGrid()
-    {
-        $testValue = 'test';
-        $testId = 777;
-
-        $questions = $this->getModelMock('oggetto_faq/questions', ['getId']);
-
-        $questions->expects($this->once())
-            ->method('getId')
-            ->willReturn($testId);
-
-        $block = $this->getBlockMock('oggetto_faq/adminhtml_faq_grid', ['getUrl']);
-
-        $block->expects($this->once())
-            ->method('getUrl')
-            ->with(
-                $this->equalTo('*/*/edit'),
-                $this->equalTo(['id' => $testId])
-            )
-            ->willReturn($testValue);
-
-        $this->assertEquals($testValue, $block->getRowUrl($questions));
+        $this->assertEquals($this->expected()->getColumns(), $blockColumns);
     }
 }
